21da9fb5bd73ce09c99576fdc104ee63b313f526
fedytion <fedyshysm@gmail.com>
2025-06-03 19:46:05 +0300

    add RouteParser to extract route info from HTML

--- src/main/java/com/fedytion/parser/RouteInfo.java
+++ src/main/java/com/fedytion/parser/RouteInfo.java
@@ -1,4 +1,4 @@
 package com.fedytion.parser;
 
-public record RouteInfo(String fromCity, String toCity) {
+public record RouteInfo(String fromCity, String toCity, String fromCityIndex, String toCityIndex) {
 }
--- src/main/java/com/fedytion/parser/RouteParser.java
+++ src/main/java/com/fedytion/parser/RouteParser.java
@@ -1,8 +1,20 @@
 package com.fedytion.parser;
 
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 public class RouteParser {
 
-    public RouteInfo getRoute() {
-        return new RouteInfo("Lviv", "Berlin");
+    public RouteInfo getRoute(Document document) {
+        Element fromCity = document.select("span[data-testid=LoadingPlacesView/loadingPlaceCity_0]").getFirst();
+        Element toCity = document.select("span[data-testid=LoadingPlacesView/loadingPlaceCity_1]").getFirst();
+
+
+        String fromCityIndex = fromCity.text().substring(2, 7);
+        String toCityIndex = toCity.text().substring(2, 7);
+        String fromCityName = fromCity.text().substring(8);
+        String toCityName = toCity.text().substring(8);
+
+        return new RouteInfo(fromCityName, toCityName, fromCityIndex, toCityIndex);
     }
 }
